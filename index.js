25d48969295d685241ee4ffcbcd2abfd8158acd7
jasnmar <jasnmar@gmail.com>
2024-04-17 13:00:46 -0400

    Lists

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
 
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 
-ReactDOM.render(<h1>Hello, Everyone</h1>, document.getElementById("root"))
-
+ReactDOM.render(<ul><li>first Line</li><li>Second Line</li></ul>, document.getElementById("root"))
