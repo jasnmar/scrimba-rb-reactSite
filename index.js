469b82b1ea737005421dd1115403cae1f67e2ca1
jasnmar <jasnmar@gmail.com>
2024-04-17 13:18:14 -0400

    Back to react

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ appending an h1 to our div#root (without using innerHTML).
 */
 
 
-const h1 = document.createElement('h1')
-h1.textContent = "I'm learning React"
-h1.classList.add('header')
-document.getElementById('root').appendChild(h1)
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
