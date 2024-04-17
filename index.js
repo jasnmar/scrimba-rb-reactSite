5adfc1c312bb155089724dd484745e26c1b6acf1
jasnmar <jasnmar@gmail.com>
2024-04-17 14:29:15 -0400

    New challenge

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
 
-We'll be adding styling to it later.
+/**
+Challenge: 
 
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
+Part 1: Create a page of your own using a custom Page component
+
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
+
+Render your list to the page
+
  */
 
-const page = (
-    <div>
-        <img width="100px" height="100px" src="react-logo.png"/>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was origninally created by Jordan Walke</li>
-            <li>Has well over 100k start on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps including mobile apps</li>
-        </ul>
-    </div>
-)
+function Page() {
+    return (
+        <ol>
+            <li>Jobs</li>
+            <li>Update my Site</li>
+            <li>Fun</li>
+        </ol>
+    )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
