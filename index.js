74882db3cbfbb81e76d1dc7367fdf18adde1a69b
jasnmar <jasnmar@gmail.com>
2024-04-17 14:11:30 -0400

    Not working, but works on scrimba

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+import React from "react"
+import ReactDOM from "react-dom"
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
 
-/* 
-Challenge: 
+We'll be adding styling to it later.
 
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
 
-const nav = (
-    <nav>
-        <h1>Website</h1>
+const page = (
+    <div>
+        <img width="100px" height="100px" src="react-logo.png"/>
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was origninally created by Jordan Walke</li>
+            <li>Has well over 100k start on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
-const root = document.getElementById("root")
-ReactDOM.render(nav, root)
 
+ReactDOM.render(page, document.getElementById("root"))
